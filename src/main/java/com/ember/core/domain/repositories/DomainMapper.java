cb0ea18f258ecfde4ba8b5f072236b7119e41676
Gabriel Barbosa <gabrielturing@gmail.com>
2024-10-26 14:03:46 -0300

    refactor: remove unused code

--- src/main/java/com/ember/core/domain/repositories/DomainMapper.java
+++
@@ -1,8 +0,0 @@
-package com.ember.core.domain.repositories;
-
-public interface DomainMapper<TDomain, TEntity> {
-
-  TDomain toDomain(TEntity entityUpdated);
-
-  TEntity toRepository(TDomain domain);
-}
